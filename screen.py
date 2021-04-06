c805e09acefd51082fa619a32f6de3aac7daefc0
Devon Fears <devonfearssub@gmail.com>
2021-04-06 18:28:19 -0500

    More squashed syntax errors, hopefully last one.

--- screen.py
+++ screen.py
@@ -28,7 +28,7 @@ def print_main(item_list):
         cushion_top = 25
         for i in items:
             draw.rectangle((cushion_left,cushion_top,cushion_left+50,cushion_top+50), fill = epd.GRAY1, outline=epd.GRAY4)
-            draw.text((cushion_left+75, cushion_top),i,font=self.arial, fill= epd.GRAY4)
+            draw.text((cushion_left+75, cushion_top),i,font=arial, fill= epd.GRAY4)
             cushion_top += 100
         
         screen.display(screen.getbuffer(Mainimage))
@@ -53,7 +53,7 @@ def turn_in(item):
     TurninImage = Image.new('1', (epd.EPD_WIDTH,epd.EPD_HEIGHT), 255)
     draw = ImageDraw.Draw(TurninImage)
     draw.rectangle((cushion_left, 125, cushion_left+50, 175), fill = epd.GRAY1, outline= epd.GRAY4)
-    draw.text((100,125),item,font=self.arial,fill = epd.GRAY4)
+    draw.text((100,125),item,font=arial,fill = epd.GRAY4)
     draw.line((25,125,75,175), fill= epd.GRAY4)
     screen.display(screen.getbuffer(TurninImage))
     sleep(2)
