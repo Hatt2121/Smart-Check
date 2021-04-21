be2c33f2ea95fd03d3eeed7ab4823298d226804e
Devon Fears <devonfearssub@gmail.com>
2021-04-21 10:02:21 -0500

    Here is the other commit from the other branch, hopefully we don't get issues when we merge the branches.

--- mailing.py
+++ mailing.py
@@ -2,9 +2,9 @@
     The user needs to install imbox onto the raspberry pi in order to download files from the user's inbox.
     That library is amazing because it simplifies the process A LOT.
 '''
-import imbox
+import imbox #imbox is something that you need to install.
 import smtplib, ssl
-import email
+import email #also need email library which should come on every python installation... at least.
 from email.mime import multipart,text
 import os
 import traceback
@@ -24,15 +24,14 @@ def download_attachment():
         for idx, attachment in enumerate(message.attachments):
             try:
                 #att_fn = attachment.get('filename') I don't want the folder to be clogged with different files that mean similar things, it's better to call it one file and then override it instead.
-                download_path = "{}/{}".format(download_folder,"downloaded_list.txt")
-                open(download_path,"wb").write(attachment.get('content').read())
+                download_path = "{}/{}".format(download_folder,"downloaded_list.txt") #donwload file as "downloaded_list.txt"
+                open(download_path,"wb").write(attachment.get('content').read()) #open file just to read.
             except:
                 print(traceback.print_exc())
 
-        mail.mark_seen(uid)
+        mail.mark_seen(uid) #mark email as read.
 
-    #logout
-    mail.logout()
+    mail.logout() #logout.
 
 
 def return_attachment(gmail, item_dict):
@@ -54,7 +53,7 @@ def return_attachment(gmail, item_dict):
     attn = open("return_attachment.txt","r") #create attachments.
     attachment = text.MIMEText(attn.read())
     attachment.add_header('Content-Disposition','attachment',filename=attn.name)
-    mjsg.attach(attachment)
+    mjsg.attach(attachment) #attach attachment... I love documentation.
 
     try:
         context = ssl.create_default_context() #send email and exit
@@ -63,4 +62,4 @@ def return_attachment(gmail, item_dict):
         smtp.sendmail("smartcheck021@gmail.com",gmail,mjsg.as_string())
         smtp.close()
     except:
-        print("Some sort of network failure happened trying to send the eamil.")
+        print("Some sort of network failure happened trying to send the eamil.") #If you get this it means you have problems with either the network or the 'gmail' (email) input.
